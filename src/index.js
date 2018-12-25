3db535cdb23d560637bef43bf7484da5f33d650f
junchang.ma.ele_waimai <junchang.ma@ele.me>
2018-12-25 21:08:09 +0800

    增加antd布局

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom'
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route path='/' component={App} exact />
+
+  </Router>,
   document.getElementById('root'))
 registerServiceWorker()
 
